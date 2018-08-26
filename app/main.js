6b688461a8eee6f3695d48437c1f820d061fad07
jlyon <jml6487@gmail.com>
2018-08-25 21:45:21 -0400

    add main.js

--- app/main.js
+++ app/main.js
@@ -1,3 +1,18 @@
-module.exports.loop = () => {
+var roleHarvester = require('./creep/harvester')
+var roleUpgrader = require('./creep/upgrader')
+var roleBuilder = require('./creep/builder')
 
+module.exports.loop = function () {
+  for (var name in Game.creeps) {
+    var creep = Game.creeps[name]
+    if (creep.memory.role === 'harvester') {
+      roleHarvester.run(creep)
+    }
+    if (creep.memory.role === 'upgrader') {
+      roleUpgrader.run(creep)
+    }
+    if (creep.memory.role === 'builder') {
+      roleBuilder.run(creep)
+    }
+  }
 }
