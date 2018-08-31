548e16473ce784edbdb9b1e9744ac8535d762f30
jlyon <jml6487@gmail.com>
2018-08-31 00:36:13 -0400

    slim the rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,6 @@
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 
-// `npm run build` -> `production` is true
-// `npm run dev` -> `production` is false
-const production = !process.env.ROLLUP_WATCH;
-
 export default {
   input: 'src/main.js',
   output: {
